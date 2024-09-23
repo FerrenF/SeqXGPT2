8e467f34b7ef93cfbe6a86e5708488353a698159
Hunter W. <kfateweaver@gmail.com>
2024-09-23 01:53:10 -0400

    Update gen_features.py

--- gen_features.py
+++ gen_features.py
@@ -31,6 +31,7 @@ class SeqXFeatureGenerator:
         :return:
         """
         with httpx.Client(timeout=None) as client:
+            print(text + type(text).__name__)
             post_data = {
                 "text": text,
                 "do_generate": do_generate,
